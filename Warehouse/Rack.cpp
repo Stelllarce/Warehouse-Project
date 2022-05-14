355861db9165ee878c3a75f7633cd9b41871cb68
Andrew <andrey.etodorov@gmail.com>
2022-05-14 22:13:11 +0300

    added move constr and op=

--- Warehouse/Rack.cpp
+++ Warehouse/Rack.cpp
@@ -1,8 +1,11 @@
+#include "Shelf.h"
 #include "Rack.h"
 
 int Rack::shelfCapacity = 0;
 
-Rack::Rack(char name): name{name}, haveSpace(shelfCapacity) {
+Rack::Rack(): shelfs(Vector<Shelf>()), name(0), haveSpace(shelfCapacity) {}
+
+Rack::Rack(unsigned int name): name{name}, haveSpace(shelfCapacity) {
 
     int index = 1;
     for (int i = 0; i < shelfCapacity; i++)
@@ -12,6 +15,28 @@ Rack::Rack(char name): name{name}, haveSpace(shelfCapacity) {
         index++;
     }
     
+}
+
+Rack::Rack(const Rack& r): name{r.name}, haveSpace{r.haveSpace} {
+
+    shelfs = r.shelfs;
+}
+
+Rack::Rack(Rack&& r): name{r.name}, haveSpace{r.haveSpace}, shelfs{r.shelfs} {
+
+    r.shelfs = Vector<Shelf>();
+    r.name = 0;
+    r.haveSpace = 0;
+}
+Rack& Rack::operator=(const Rack& r) {
+
+    if (this != &r)
+    {
+        name = r.name;
+        haveSpace = r.haveSpace;
+        shelfs = r.shelfs;
+    }
+    return *this;
 }
 
 void Rack::spaceCheck() {
--- Warehouse/Rack.h
+++ Warehouse/Rack.h
@@ -1,22 +1,27 @@
 #ifndef RACK
 #define RACK
-#include "Shelf.h"
 #include "../Vector/Vector.h"
 #include "Warehouse.h"
+#include "Shelf.h"
+class Shelf; 
 class Rack {
 
     private:
-    Shelf a;
+    
     Vector<Shelf> shelfs;
-    char name;
+    unsigned int name;
     unsigned int haveSpace;
     static int shelfCapacity;
     
     public:
 
-    Rack(char name);
+    Rack();
+    Rack(unsigned int name);
+    Rack(const Rack&);
+    Rack(Rack&&);
+    Rack& operator=(const Rack&);
 
-    bool  isRFull() const;
+    bool isRFull() const;
     void spaceCheck();
     
     friend class Warehouse;
